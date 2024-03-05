4a5042dbe0ad931fb81772cc70e4e9994164d98d
Alessandro Lelli <alessandro.lelli.external@ynap.com>
2024-03-05 15:19:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+
+const cinemaId = "4" /* Meridiana - Bologna */
+const apiUrl = `https://www.ucicinemas.it/rest/v3/cinemas/${cinemaId}/programming`;
+const scrapedDataFolderPath = "scraped-data";
+const updatesFolderPath = "differences-data";
+const bearerToken = "SkAkzoScIbhb3uNcGdk8UL0XMIbvs5";
+
+async function getJSON() {
+  const { data: jsonData } = await axios.get(apiUrl, {
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+    },
+  });
+  return jsonData;
+}
+
+function saveToFile(data, filePath) {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+}
+
+function createNewStructure(apiResponse) {
+  return _.flatMap(apiResponse, (movie) => {
+    return _.flatMap(movie.events, (event) => {
+      return event.performances.map((performance) => {
+        return {
+          movieId: movie.movieId,
+          eventId: event.eventId,
+          name: movie.name,
+          isPurchasable: movie.isPurchasable,
+          firstPerformance: movie.firstPerformance,
+          date: event.date,
+          time: performance.time,
+          movieNew: event.movieNew,
+          moviePath: event.moviePath,
+          screen: performance.screen,
+          webUrl: event.webUrl,
+          buyUrl: performance.buyUrl,
+          moviePosterMedium: movie.moviePosterMedium,
+        };
+      });
+    });
+  });
+}
+
+function compareAndSaveDifferences(newStructure, penultimateFilePath, updatesFolderPath) {
+  if (fs.existsSync(penultimateFilePath)) {
+    const penultimateData = JSON.parse(fs.readFileSync(penultimateFilePath));
+    const differences = _.differenceWith(newStructure, penultimateData, _.isEqual);
+
+    if (differences.length > 0) {
+      console.log("Differences detected.");
+
+      const timestamp = new Date()
+        .toISOString()
+        .replace(/:/g, "-")
+        .replace(/\./g, "-");
+      const differencesFilePath = path.join(
+        updatesFolderPath,
+        `differences_${timestamp}.json`
+      );
+
+      fs.writeFileSync(
+        differencesFilePath,
+        JSON.stringify(differences, null, 2)
+      );
+    } else {
+      console.log("No differences found.");
+    }
+  } else {
+    console.log("No previous data for comparison.");
+  }
+}
+
+if (!fs.existsSync(scrapedDataFolderPath)) {
+  fs.mkdirSync(scrapedDataFolderPath);
+}
+if (!fs.existsSync(updatesFolderPath)) {
+  fs.mkdirSync(updatesFolderPath);
+}
+
+const timestamp = new Date()
+  .toISOString()
+  .replace(/:/g, "-")
+  .replace(/\./g, "-");
+const newScrapedDataFilePath = path.join(
+  scrapedDataFolderPath,
+  `scraped-data_${timestamp}.json`
+);
+
+getJSON().then((data) => {
+  const newStructure = createNewStructure(data);
+  saveToFile(newStructure, newScrapedDataFilePath);
+
+  const scrapedDataFiles = fs.readdirSync(scrapedDataFolderPath);
+  const sortedFiles = scrapedDataFiles
+    .map((filename) => ({
+      name: filename,
+      time: fs.statSync(path.join(scrapedDataFolderPath, filename)).birthtimeMs,
+    }))
+    .sort((a, b) => b.time - a.time);
+
+  const [latestFile, penultimateFile] = sortedFiles.slice(0, 2);
+
+  if (sortedFiles.length > 1) {
+    const penultimateFilePath = path.join(scrapedDataFolderPath, penultimateFile.name);
+    compareAndSaveDifferences(newStructure, penultimateFilePath, updatesFolderPath);
+  }
+});
